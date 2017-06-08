0e7fa7065f6d6d5b750c743912a7eeb670aae645
atiredturtle <guy@segev.com>
2017-06-08 20:00:38 +1000

    changes

--- Project/World_internet Use/generateWebCharts.js
+++ Project/World_internet Use/generateWebCharts.js
@@ -7,67 +7,15 @@ var MAX_TOP = 50;
 var myChart;
 
 // ----------------- DATA DICTS -----------------
-
-// Name: allSitesDict
+// Name: popDict
 // Key: Site Name
 // Value: Array counting popularity, indexed by popularity value. 
 //        - E.g. if the 3st item is 10, then 10 countries have the site as their 3rd favourite
-var allSitesDict = {};
+var popDict = {};
 
-function prepareCharts(){
+var viewsDict = {};
 
-    // parse data
-    Papa.parse(url, {
-        download: true,
-        header: true,
-        skipEmptyLines: true,
-        complete: function(results) {  
-            processData(results.data); // populate dicts with CSV data
-            drawChart(allSitesDict);
-            prepareSliders(); // initialise sliders
-        }
-    });
-}
-    
-// takes parsed CSV and populates dicts accordingly
-function processData(d){
-    var tempSites = [];
-    for (var i = 0; i < d.length; i++){
-        var country = d[i].country;
-        var website = d[i].Website;
-
-        // for website popularity (dict value is number of countries that have it as a fav)
-        if (website in allSitesDict){
-            if (d[i].Country_Rank in allSitesDict[website]){
-                allSitesDict[website][d[i].Country_Rank] += 1;
-            } else {
-                allSitesDict[website][d[i].Country_Rank] = 1;
-            }
-        } else {
-            allSitesDict[website] = [];
-            allSitesDict[website][d[i].Country_Rank] = 1;
-        }
-    }
-}
-
-function reduceDict(d, top){
-    var retDict = {};
-    for (var key in d){
-        // counts number of popularity votes up to the 'top'th popularity
-        // e.g if top = 4, only count the popularity from 1-4
-        var occ = 0;
-        for (var i = 1; i <= top; i++){
-            if (d[key][i] !== undefined){
-                occ += d[key][i];
-            }
-        }
-        retDict[key] = occ;
-
-    }
-    return retDict;
-}
-
-
+// ----------------- HELPER FUNCTIONS -----------------
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -81,8 +29,64 @@ function getRandomColor(){
     return 'rgba('+r+', '+g+', '+b+', 0.6)';
 }
 
-function drawChart(d, top=MAX_TOP, tail=MIN_TAIL){
-    d = reduceDict(d, top);
+// ----------------- CODE -----------------
+
+function prepareCharts(){
+
+    // parse data
+    Papa.parse(url, {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: function(results) {  
+            processData(results.data); // populate dicts with CSV data
+            drawPopChart();
+            prepareSliders(); // initialise sliders
+        }
+    });
+}
+    
+// takes parsed CSV and populates the dicts accordingly
+function processData(d){
+    var tempSites = [];
+    for (var i = 0; i < d.length; i++){
+        var item = d[i];
+        var country = item.country;
+        var website = item.Website;
+
+        // for website popularity (dict value is number of countries that have it as a fav)
+        if (website in popDict){
+            if (item.Country_Rank in popDict[website]){
+                popDict[website][item.Country_Rank] += 1;
+            } else {
+                popDict[website][item.Country_Rank] = 1;
+            }
+        } else {
+            popDict[website] = [];
+            popDict[website][item.Country_Rank] = 1;
+        }
+
+        // for website view count (dict value is avg daily visitors)
+        if (!(website in viewsDict)){
+            viewsDict[website] = d[i];
+        }
+    }
+}
+
+
+function drawPopChart(top=MAX_TOP, tail=MIN_TAIL){
+    // reduce dictionary
+    var d = {}; 
+    for (var key in popDict){
+        // counts number of popularity votes up to the 'top'th popularity
+        // e.g if top = 4, only count the popularity from 1-4
+        var occ = 0;
+        for (var i = 1; i <= top; i++){
+            if (popDict[key][i] !== undefined) occ += popDict[key][i];
+        }
+        d[key] = occ;
+    }
+
     if (myChart !== undefined) {myChart.destroy();}
     var my_keys = [];
     var my_data = [];
@@ -97,7 +101,7 @@ function drawChart(d, top=MAX_TOP, tail=MIN_TAIL){
         my_data.push(d[key]);
     }
 
-    var ctx = "myChart";
+    var ctx = "popularityChart";
     myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -143,7 +147,7 @@ function prepareSliders(){
     itemSlider.noUiSlider.on('change', function(){
         var tail = itemSlider.noUiSlider.get();
         var top  = popularitySlider.noUiSlider.get();
-	    drawChart(allSitesDict, top, tail); 
+	    drawPopChart(top, tail); 
     });
 
     var popularitySlider = document.getElementById('popularitySlider');
@@ -166,7 +170,7 @@ function prepareSliders(){
     popularitySlider.noUiSlider.on('change', function(){
         var tail = itemSlider.noUiSlider.get();
         var top  = popularitySlider.noUiSlider.get();
-        drawChart(allSitesDict, top, tail); 
+        drawPopChart(top, tail); 
     });
 
 }
